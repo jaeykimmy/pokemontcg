86e4619ce4adf990934c58dbbf19e451358b1969
Jaeyoung Kim <j.jaeykim@gmail.com>
2022-07-11 17:37:18 -0400

    price for non rare

--- src/components/PriceTable.js
+++ src/components/PriceTable.js
@@ -8,39 +8,48 @@ import Paper from "@mui/material/Paper";
 import { Grid } from "@mui/material";
 
 export default function BasicTable(props) {
+  const priceLabels = Object.keys(
+    Object.values(props.cardInfo.tcgplayer.prices)[0]
+  );
+  const priceNormal = Object.values(
+    Object.values(props.cardInfo.tcgplayer.prices)[0]
+  );
+  const priceRare = Object.values(props.cardInfo.tcgplayer.prices)[1];
   return (
-    <TableContainer component={Paper} >
-      {Object.keys(props.cardInfo.tcgplayer.prices).map((x) => <p>{x.charAt(0).toUpperCase() + x.slice(1)}</p>)}
+    <TableContainer component={Paper}>
+      {/* {Object.keys(Object.values(props.cardInfo.tcgplayer.prices)[0]).map((x) => <p>{x}</p>)}
+      {Object.values(Object.values(props.cardInfo.tcgplayer.prices)[0]).map((x) => <p>{x}</p>)}
+      {Object.values(props.cardInfo.tcgplayer.prices)[1] && Object.values(Object.values(props.cardInfo.tcgplayer.prices)[1]).map((x) => <p>{x}</p>)}
+       */}
+
       <Grid item sm={12}>
-        <Table sx={{ minWidth: 360 }} aria-label="simple table" key={props.cardInfo.id}>
-          <TableBody >
+        <Table
+          sx={{ minWidth: 360 }}
+          aria-label="simple table"
+          key={props.cardInfo.id}
+        >
+          <TableBody>
             <TableRow>
-              {Object.values(props.cardInfo.tcgplayer.prices).map((x) =>
-                Object.keys(x).map((x) => (
-                  <TableCell variant="head" align="right">
-                    {(x.charAt(0).toUpperCase() + x.slice(1))
-                      .replace(/([A-Z])/g, " $1")
-                      .trim()}
-                    
-                  </TableCell>
-                ))
-              )}
+              {priceLabels.map((x) => (
+                <TableCell variant="head" align="right">
+                  {x}
+                </TableCell>
+              ))}
             </TableRow>
 
             <TableRow
               sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
             >
-              {Object.values(props.cardInfo.tcgplayer.prices).map((x) =>
-                Object.values(x).map((x) => (
-                  <TableCell variant="head" align="right">
-                    ${x.toFixed(2)}
-                  </TableCell>
-                ))
-              )}
+              {priceNormal.map((x) => (
+                <TableCell variant="head" align="right">
+                  ${x.toFixed(2)}
+                </TableCell>
+              ))}
             </TableRow>
           </TableBody>
         </Table>
       </Grid>
+      
     </TableContainer>
   );
 }
