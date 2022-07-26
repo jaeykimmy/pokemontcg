50c2cf455331938b97bd62e1a1b8875e4b79f0ec
Jaeyoung Kim <j.jaeykim@gmail.com>
2022-07-26 14:15:25 -0400

    format to capitalize first letter of each word in prices, camelcase words have space

--- src/components/PriceTable.js
+++ src/components/PriceTable.js
@@ -14,7 +14,7 @@ export default function BasicTable(props) {
   const priceNormal = Object.values(
     Object.values(props.cardInfo.tcgplayer.prices)[0]
   );
-  
+
   return (
     <TableContainer component={Paper}>
       {/* {Object.keys(Object.values(props.cardInfo.tcgplayer.prices)[0]).map((x) => <p>{x}</p>)}
@@ -32,7 +32,11 @@ export default function BasicTable(props) {
             <TableRow>
               {priceLabels.map((x) => (
                 <TableCell variant="head" align="right">
-                  {x}
+                  {x.charAt(0).toUpperCase() +
+                    x
+                      .slice(1)
+                      .replace(/([A-Z])/g, " $1")
+                      .trim()}
                 </TableCell>
               ))}
             </TableRow>
@@ -49,35 +53,43 @@ export default function BasicTable(props) {
           </TableBody>
         </Table>
       </Grid>
-      {Object.values(props.cardInfo.tcgplayer.prices)[1] && <Grid item sm={12}>
-        <Table
-          sx={{ minWidth: 360 }}
-          aria-label="simple table"
-          key={props.cardInfo.id}
-        >
-          <TableBody>
-            <TableRow>
-              {Object.keys(Object.values(props.cardInfo.tcgplayer.prices)[1]).map((x) => (
-                <TableCell variant="head" align="right">
-                  {x}
-                </TableCell>
-              ))}
-            </TableRow>
+      {Object.values(props.cardInfo.tcgplayer.prices)[1] && (
+        <Grid item sm={12}>
+          <Table
+            sx={{ minWidth: 300 }}
+            aria-label="simple table"
+            key={props.cardInfo.id}
+          >
+            <TableBody>
+              <TableRow>
+                {Object.keys(
+                  Object.values(props.cardInfo.tcgplayer.prices)[1]
+                ).map((x) => (
+                  <TableCell variant="head" align="right">
+                    {x.charAt(0).toUpperCase() +
+                      x
+                        .slice(1)
+                        .replace(/([A-Z])/g, " $1")
+                        .trim()}
+                  </TableCell>
+                ))}
+              </TableRow>
 
-            <TableRow
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
-              {Object.values(Object.values(props.cardInfo.tcgplayer.prices)[1]).map((x) => (
-                <TableCell variant="head" align="right">
-                  ${x.toFixed(2)}
-                </TableCell>
-              ))}
-            </TableRow>
-          </TableBody>
-        </Table>
-      </Grid>}
-      
-      
+              <TableRow
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                {Object.values(
+                  Object.values(props.cardInfo.tcgplayer.prices)[1]
+                ).map((x) => (
+                  <TableCell variant="head" align="right">
+                    ${x.toFixed(2)}
+                  </TableCell>
+                ))}
+              </TableRow>
+            </TableBody>
+          </Table>
+        </Grid>
+      )}
     </TableContainer>
   );
 }
